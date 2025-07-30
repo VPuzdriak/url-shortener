07ea93c2cc5bdd8e14d16b3c495c1d6c10eac591
vpuzdriak <vpuzdriak@independer.nl>
2025-07-30 15:27:11 +0200

    Generate unique tokens in parallel

--- Api/src/UrlShortener.Core/TokenProvider.cs
+++ Api/src/UrlShortener.Core/TokenProvider.cs
@@ -2,17 +2,26 @@
 
 public class TokenProvider
 {
+    private readonly object _tokenLock = new();
+    private long _token = 0;
     private TokenRange? _tokenRange;
 
     public void AssignRange(int start, int end)
     {
-        _tokenRange = new TokenRange(start, end);
+        AssignRange(new TokenRange(start, end));
     }
-    
+
     public void AssignRange(TokenRange tokenRange)
     {
         _tokenRange = tokenRange;
+        _token = tokenRange.Start;
     }
 
-    public long GetToken() => _tokenRange.Start;
+    public long GetToken()
+    {
+        lock (_tokenLock)
+        {
+            return _token++;
+        }
+    }
 }
